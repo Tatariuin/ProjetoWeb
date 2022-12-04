880c1bc88d0ad8d3bec5cec1db8816d890c857b1
Tatariuin <rogeriopalafoz@gmail.com>
2022-12-04 10:53:07 -0300

    versão/04-12-22

--- js/scriptAddButtons.js
+++ js/scriptAddButtons.js
@@ -2,8 +2,8 @@ var cap = document.getElementById("capitulos")
 var p = document.getElementsByTagName("p")
 var span = document.getElementsByTagName("span")
 var defineTamanho = 16
-for(var i = 1;i <= 83;i++){
-    cap.innerHTML += "<button style='border-radius:15px;margin-top:10px;margin-left:26px;width: 130px;height:35px'>capitulo "+i+"</button>"
+for(var i = 1;i <= 87;i++){
+    cap.innerHTML += "<a href='Mangas.html'><button style='border-radius:15px;margin-top:10px;margin-left:26px;width: 130px;height:35px'>capitulo "+i+"</button></a>"
     
 }
 
---
+++ js/scriptAssButtons.js
@@ -0,0 +1,8 @@
+leitorManga = document.getElementById("imgsLeitorManga")
+
+for(var i = 1;i <= 16;i++){
+
+    if(i != 3){
+    leitorManga.innerHTML += "<img src='../img/MangaOrigin/"+i+"-o.jpg' style='margin:0 auto;width:100%'>"
+    }
+}
