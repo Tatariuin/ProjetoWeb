1e0c6b7df8cb28b9a1b6ee5bc01a1886113a93cd
Tatariuin <rogeriopalafoz@gmail.com>
2022-12-05 10:19:33 -0300

    versão/05-12-22

--- js/scriptAddButtons.js
+++ js/scriptAddButtons.js
@@ -2,9 +2,12 @@ var cap = document.getElementById("capitulos")
 var p = document.getElementsByTagName("p")
 var span = document.getElementsByTagName("span")
 var defineTamanho = 16
-for(var i = 1;i <= 87;i++){
-    cap.innerHTML += "<a href='Mangas.html'><button style='border-radius:15px;margin-top:10px;margin-left:26px;width: 130px;height:35px'>capitulo "+i+"</button></a>"
-    
+
+function addButtons(){
+    for(var i = 1;i <= 87;i++){
+        cap.innerHTML += "<a href='Mangas.html'><button style='border-radius:15px;margin-top:10px;margin-left:26px;width: 130px;height:35px'>capitulo "+i+"</button></a>"
+        
+    }
 }
 
 function aumentaTexto(){
